d6ec2d0aeeef760665dd6a7cc509212372d74ad1
code_babee <98746901+codebabee@users.noreply.github.com>
2022-09-14 08:52:05 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+switch (card){
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+  count++;
+  break;
+  case 7:
+  case 8:
+  case 9:
+  break;
+  case 10:
+  case 'J':
+  case 'Q':
+  case 'K':
+  case 'A':
+  count--;
+  break; 
+}
+ if (count > 0){
+   console.log(count + " Bet");
+ } 
+  else {
+    console.log( count + " Hold");
+  }
+  return "Change Me";
+  // Only change code above this line
+}
+cc(2); cc(3); cc(4); cc(5); cc(6);
